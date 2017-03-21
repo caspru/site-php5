8ea8ed5ac492cfbc14a3f30b928a0116e4b5e5be
Oleg Sidenko <oleg@casp.ru>
2017-03-21 18:32:54 +0300

    php5.6.31-commit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R 33:33 /home/sftpdev/ && chmod 700 /home/sftpdev/
+chown -R 33:33 /home/sftpdev/ /var/www/html ; chmod 700 /home/sftpdev/
 chown root:root /var/spool/cron/ && chmod 755 /var/spool/cron/
 /usr/bin/python /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
